c64656a9001fbef133c6983ec20ee7bad105e692
nilantha1989 <nilantha.sac@gmail.com>
2019-08-18 03:37:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using _7ShiftTest.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _7ShiftTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorService cal = new CalculatorService();
+
+            Console.Write("Enter input string: ");
+           
+            string input = Console.ReadLine();
+           
+            cal.Add(input);
+
+        }
+    }
+}
